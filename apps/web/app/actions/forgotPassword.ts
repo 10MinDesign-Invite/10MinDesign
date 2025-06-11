26cf16c0a25284587c797c6709ec8e66d2da90b8
10MinDesign-Invite <10mindesign.invite@gmail.com>
2025-06-11 22:33:36 +0530

    testt

--- apps/web/app/actions/forgotPassword.ts
+++ apps/web/app/actions/forgotPassword.ts
@@ -20,7 +20,7 @@ export async function forgotPassword(formData: FormData, otp: string) {
         email,
         password,
         confirmPassword,
-        token: myCookie?.value,
+        token: myCookie?.value.toString(),
       }),
     });
 
