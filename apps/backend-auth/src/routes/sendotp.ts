33f71cb8ddea4acb9ad68a9213b9cb2cbac697be
10MinDesign-Invite <10mindesign.invite@gmail.com>
2025-06-11 17:49:58 +0530

    chenge cookie samesite to lax

--- apps/backend-auth/src/routes/sendotp.ts
+++ apps/backend-auth/src/routes/sendotp.ts
@@ -156,7 +156,7 @@ OTP.post("/verify-otp", async (req, res) => {
         res.cookie("token", token, {
           httpOnly: true,
           secure: process.env.NODE_ENV == "production" ? true : false,
-          sameSite: process.env.NODE_ENV == "production" ? "none" : "lax",
+          sameSite:"lax",
           path: "/",
           maxAge: 5 * 60 * 1000,
         });
