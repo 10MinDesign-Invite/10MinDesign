81ef6c9ed88baf4cedd561f64619404dd8eef88e
10MinDesign-Invite <10mindesign.invite@gmail.com>
2025-07-13 23:40:08 +0530

    fix feature responsive issue

--- apps/web/app/(home)/components/Features.tsx
+++ apps/web/app/(home)/components/Features.tsx
@@ -12,7 +12,7 @@ export function Features() {
                 <div className="w-[50%] sm:w-[400px] lg:w-[600px] mx-auto mt-7 text-4xl sm:text-5xl lg:text-6xl text-center font-extrabold  flex justify-center items-center">
                     Made Easy Save Time
                 </div>
-                <div className=" mt-7 max-w-[330px] lg:w-[400px] mx-auto outline">
+                <div className=" mt-7 max-w-[330px] lg:w-[400px] mx-auto">
                     <p className="text-center text-[16px] lg:text-[20px] lg:w-[90%]">
                          Easily design, select a brand logo,
                           fill in the details, download your design, and share it
