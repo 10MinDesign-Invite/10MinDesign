3efd084f6016e75c22bf94b9857e8b65c8f5bb8c
10MinDesign-Invite <10mindesign.invite@gmail.com>
2025-08-17 15:56:56 +0530

    remove the .env and dist

--- packages/database/dist/client.js
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from "../generated/client/index.js";
-var globalForPrisma = global;
-export var prisma = globalForPrisma.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production")
-    globalForPrisma.prisma = prisma;
-// export * from "../generated/client/index.js";
