c8512f980d49f6e4028a9a2fa9eb3038c286ee3b
10MinDesign-Invite <10mindesign.invite@gmail.com>
2025-09-30 04:38:05 +0530

    adding  forgot password route

--- apps/backend-auth/src/index.ts
+++ apps/backend-auth/src/index.ts
@@ -2,37 +2,35 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import * as dotenv from 'dotenv';
 import express from "express";
+import {auth, toNodeHandler} from "@repo/auth/auth"
 // import { addUser } from "./routes/addUser";
-// import { OTP } from "./routes/sendotp";
+import { OTP } from "./routes/sendotp";
 // import { verify_Add_User } from "./routes/verify-Add-User";
 // import { getUsers } from "./routes/getUsers";
 import job from "./config/cron";
-import { auth, toNodeHandler } from "@repo/better-auth/auth";
-
 
 
 dotenv.config();
 const app = express();
+app.use(cookieParser());
 
 app.use(cors({
-    origin:[process.env.FRONTEND_URL!,process.env.CORN_JOB!],
+    origin:[`http://localhost:3000`,process.env.CORN_JOB!],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
 
-app.all("/api/auth/*splat", toNodeHandler(auth));
+app.all('/api/auth/*spalt', toNodeHandler(auth));
 
 app.use(express.json());
-app.use(cookieParser());
 if(process.env.NODE_ENV == "production") job.start();
 
 app.get("/api/health", (req,res)=>{
     res.status(200).json({status: "ok"});
 });
-// app.use("/auth",OTP);
+app.use("/auth",OTP);
 // app.use("/verify",verify_Add_User);
 // app.use("/add",addUser);
 // app.use("/get", getUsers);
-
 
 app.listen(process.env.PORT || 8080);
