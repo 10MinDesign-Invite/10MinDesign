d9d99beef1c90c20f27530f6d207d6df648e9a80
10MinDesign-Invite <10mindesign.invite@gmail.com>
2025-09-30 04:28:46 +0530

    adding successfully password resset with betterauth

--- packages/auth/src/lib/auth.ts
+++ packages/auth/src/lib/auth.ts
@@ -3,6 +3,8 @@ dotenv.config();
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import {prisma} from "@repo/database"
+import { customSession, emailOTP } from 'better-auth/plugins';
+import axios from 'axios';
 
 export const auth = betterAuth({
   secret: `${process.env.BETTER_AUTH_SECRET}`,
@@ -24,7 +26,50 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
-  
+  // plugins: [
+  //       customSession(async ({ user, session }) => {
+  //         const dbUser = await prisma.user.findUnique({
+  //       where: { id: user.id },
+  //       select: {       
+  //         role: true 
+  //       },
+  //     });
+  //           return {
+  //               user: {
+  //                   ...user,
+  //                   role: dbUser.,
+  //               },
+  //               session
+  //           };
+  //       }),
+  //   ],
+
+
+
+
+    plugins: [
+        emailOTP({ 
+            async sendVerificationOTP({ email, otp, type }) { 
+                if (type === "sign-in") { 
+                    // Send the OTP for sign in
+                } else if (type === "email-verification") { 
+                    // Send the OTP for email verification
+                } else { 
+                  
+                    await axios.post(`${process.env.BACKEND_URL}/auth/send-otp`,{email,otp,type})
+
+                } 
+            }, 
+        }) 
+    ]
+
+
+
+
+
+
+
+
 });
 
 export {toNodeHandler} from "better-auth/node"
--- packages/auth/src/lib/authClient.ts
+++ packages/auth/src/lib/authClient.ts
@@ -1,7 +1,12 @@
+import { emailOTPClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
     baseURL: process.env.NEXT_PUBLIC_Backend_URL as string, 
+    // email otp
+    plugins: [
+        emailOTPClient()
+    ]
 });
 
 export {toNextJsHandler} from "better-auth/next-js"
