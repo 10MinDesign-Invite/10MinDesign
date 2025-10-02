77c6728c6149cb005bcb07584350f6e0d4e00873
10MinDesign-Invite <10mindesign.invite@gmail.com>
2025-10-02 22:48:54 +0530

    index.ts

--- apps/backend-auth/src/index.ts
+++ apps/backend-auth/src/index.ts
@@ -3,10 +3,8 @@ import cors from "cors";
 import * as dotenv from 'dotenv';
 import express from "express";
 import {auth, toNodeHandler} from "@repo/auth/auth"
-// import { addUser } from "./routes/addUser";
 import { OTP } from "./routes/sendotp";
-// import { verify_Add_User } from "./routes/verify-Add-User";
-// import { getUsers } from "./routes/getUsers";
+import { getUsers } from "./routes/getUsers";
 import job from "./config/cron";
 
 
@@ -15,7 +13,7 @@ const app = express();
 app.use(cookieParser());
 
 app.use(cors({
-    origin:[`http://localhost:3000`,process.env.CORN_JOB!],
+    origin:[`${process.env.FRONTEND_URL}`,process.env.CORN_JOB!],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
@@ -28,9 +26,8 @@ if(process.env.NODE_ENV == "production") job.start();
 app.get("/api/health", (req,res)=>{
     res.status(200).json({status: "ok"});
 });
+
 app.use("/auth",OTP);
-// app.use("/verify",verify_Add_User);
-// app.use("/add",addUser);
-// app.use("/get", getUsers);
+app.use("/get", getUsers);
 
 app.listen(process.env.PORT || 8080);
