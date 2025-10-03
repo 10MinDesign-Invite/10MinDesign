25370de20c56e0ac291f5c6a9a67acc30e025345
10MinDesign-Invite <10mindesign.invite@gmail.com>
2025-10-04 00:26:04 +0530

    testing

--- packages/auth/src/lib/authClient.ts
+++ packages/auth/src/lib/authClient.ts
@@ -1,7 +1,6 @@
 import { customSessionClient, emailOTPClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react"
 import type {auth} from "./auth.js"
-console.log(process.env.NEXT_PUBLIC_Backend_URL,"authClient=====")
 export const authClient = createAuthClient({
     baseURL: process.env.NEXT_PUBLIC_Backend_URL as string, 
     // email otp
