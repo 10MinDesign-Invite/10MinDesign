dbc18478e523e2bdf57a30ba5440a566690aae19
10MinDesign-Invite <10mindesign.invite@gmail.com>
2025-10-11 21:10:41 +0530

    test2

--- packages/database/src/lib/auth.ts
+++ packages/database/src/lib/auth.ts
@@ -17,8 +17,8 @@ export const auth = betterAuth({
 
   socialProviders: {
     google: {
-      clientId: `${process.env.AUTH_GOOGLE_ID}`,
-      clientSecret: `${process.env.AUTH_GOOGLE_SECRET}`,
+      clientId: `${process.env.AUTH_GOOGLE_ID!}`,
+      clientSecret: `${process.env.AUTH_GOOGLE_SECRET!}`,
     },
   },
 });
