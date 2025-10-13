a6fda53497513438b6865adc78c7b4a101be43db
10MinDesign-Invite <10mindesign.invite@gmail.com>
2025-10-13 19:16:27 +0530

    remove nodemailer

--- apps/backend-auth/src/config/resend-config.ts
+++ apps/backend-auth/src/config/resend-config.ts
@@ -1,3 +1,3 @@
 import { Resend } from "resend";
 
-export const resend = new Resend(process.env.RESEND_API_KEY_DEV);
+export const resend = new Resend(process.env.NODE_ENV== "development" ? process.env.RESEND_API_KEY : process.env.RESEND_API_KEY_DEV);
