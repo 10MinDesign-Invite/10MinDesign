55af025c5bd3c5c193c6ef205dddacb164de9049
10MinDesign-Invite <10mindesign.invite@gmail.com>
2025-11-22 21:27:08 +0530

    adding database with prisma.config.ts and add genarte with --config and --schema

--- packages/database/prisma/migrations/20251018135534_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `expireAt` on the `OtpStore` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "OtpStore" DROP COLUMN "expireAt",
-ADD COLUMN     "expiresAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
--- packages/database/prisma/migrations/20251120130841_adding_index_google_id/migration.sql
+++
@@ -1,2 +0,0 @@
--- CreateIndex
-CREATE INDEX "User_googleId_idx" ON "User"("googleId");
