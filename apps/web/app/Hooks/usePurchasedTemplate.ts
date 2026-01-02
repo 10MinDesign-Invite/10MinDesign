b5a68662c046a8ce7c17917e714278ab330fe4af
10MinDesign-Invite <10mindesign.invite@gmail.com>
2026-01-03 02:25:04 +0530

    added trycatch

--- apps/web/app/Hooks/usePurchasedTemplate.ts
+++ apps/web/app/Hooks/usePurchasedTemplate.ts
@@ -5,9 +5,13 @@ import { useState } from "react";
 export function usePurchesedTemplate(templateName:string){
     const [purchased, setPurchased] = useState(false);
     async function getCookie(templateName:string){
-        const result = await axios.post(`${process.env.NEXT_PUBLIC_Backend_URL}/purchased/template`,{templateName},{withCredentials:true})
-        if(result.status === 200){
-            setPurchased(true)
+        try {
+            const result = await axios.post(`${process.env.NEXT_PUBLIC_Backend_URL}/purchased/template`,{templateName},{withCredentials:true})
+            if(result.status === 200){
+                setPurchased(true)
+            }
+        } catch (error) {
+            
         }
     }
     getCookie(templateName);
